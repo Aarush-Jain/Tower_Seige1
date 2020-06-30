69d5eef1b72150740c706bf784bea5af33d5fff0
Aarush-Jain <ajjacky1909@gmail.com>
2020-06-30 23:52:41 +0530

    committed

--- sketch.js
+++ sketch.js
@@ -1,9 +1,20 @@
+var stand;
+
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  engine = Engine.create;
+  world = engine.world;
+  stand = new Ground(390,265,150,20);
+  Engine.run(engine);
 }
 
 function draw() {
-  background(255,255,255);  
-  drawSprites();
+  background(255,255,255); 
+  stand.display();
 }
